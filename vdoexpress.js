75b753107f704332ca3895226bc69e40662064b8
ChinmayLale <chinmay29.lale@gmail.com>
2024-04-04 15:35:23 +0530

    first commit

---
+++ vdoexpress.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+const emailToSocketId = new Map();
+const socketToEmail = new Map();
+
+io.on('connection', (socket) => {
+    console.log(`Socket Connected ${socket.id}`);
+
+    socket.on('room:join', (data) => {
+        console.log(data);
+        const { email, room } = data;
+        emailToSocketId.set(email, socket.id);
+        socketToEmail.set(socket.id, email);
+
+        io.to(room).emit('user:joined', { email, id: socket.id });
+        socket.join(room);
+        io.to(socket.id).emit('room:join', data);
+    });
+
+    // Other event handlers (user:call, call:accepted, peer:nego:needed, peer:nego:done) go here...
+
+    // Example:
+    socket.on('user:call', ({ to, offer }) => {
+        io.to(to).emit('incomming:call', { from: socket.id, offer });
+    });
+
+    socket.on('call:acepted',({to , ans})=>{
+        io.to(to).emit('call:acepted',{from : socket.id , ans});
+        console.log("Call Accepted");
+    });
+
+    socket.on('peer:nego:needed' ,({to , offer})=>{
+        io.to(to).emit('peer:nego:needed',{from : socket.id , offer});
+        console.log("Nego recived Done")
+    });
+
+    socket.on('peer:nego:done',({to , ans})=>{
+        io.to(to).emit('peer:nego:final',{from : socket.id , ans});
+        console.log("Nego Send Done")
+    });
+});
+
+const PORT = process.env.PORT || 8001;
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+    // document.write('<h1>Hiii </h1>'); 
+});
---
+++ video.js
@@ -0,0 +1,48 @@
+const {Server } = require('socket.io');
+
+const io = new Server(8001 , {
+    cors : true
+});
+
+
+const emailToSocketId = new Map();
+const socketToEmail = new Map();
+
+
+
+
+io.on("connection",(socket) => {
+    console.log(`Socket Connected ${socket.id}`);
+    socket.on('room:join',data =>{
+        console.log(data);
+        const{email , room} = data;
+        emailToSocketId.set(email , socket.id);
+        socketToEmail.set(socket.id , email);
+
+
+        io.to(room).emit('user:joined',{email , id:socket.id});
+        socket.join(room);
+        io.to(socket.id).emit('room:join',data);
+    })
+
+    socket.on('user:call',({to , offer})=>{
+        io.to(to).emit('incomming:call',{from : socket.id , offer});
+    })
+
+    socket.on('call:acepted',({to , ans})=>{
+        io.to(to).emit('call:acepted',{from : socket.id , ans});
+        console.log("Call Accepted");
+    })
+
+    socket.on('peer:nego:needed' ,({to , offer})=>{
+        io.to(to).emit('peer:nego:needed',{from : socket.id , offer});
+        console.log("Nego recived Done")
+    })
+
+    socket.on('peer:nego:done',({to , ans})=>{
+        io.to(to).emit('peer:nego:final',{from : socket.id , ans});
+        console.log("Nego Send Done")
+    })
+})
+
+console.log('ALl GOod');
